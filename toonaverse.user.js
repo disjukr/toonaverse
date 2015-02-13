9c89755b6fe4415a44ceec853c53539670d2c3b1
JongChan Choi <disjukr@naver.com>
2015-02-14 02:07:45 +0900

    only current contents

--- toonaverse.user.js
+++ toonaverse.user.js
@@ -12,6 +12,10 @@ var toonaverse = {
     dom: function (url) {
         var self = this;
         return new Promise(function (resolve, reject) {
+            if (url === undefined) {
+                resolve(document);
+                return;
+            }
             var xhr = new XMLHttpRequest;
             xhr.open('get', url);
             xhr.onload = function () {
@@ -27,10 +31,28 @@ var toonaverse = {
     content: function (titleId, no) {
         var self = this;
         return new Promise(function (resolve, reject) {
-            self.dom('http://comic.naver.com/webtoon/detail.nhn' + self.searchStr({
-                titleId: titleId || self.search.titleId,
-                no: no || self.search.no
-            })).then(function (contentDom) {
+            var url = new Promise(function (resolve, reject) {
+                if (titleId === undefined) {
+                    resolve(undefined);
+                    return;
+                }
+                function compose(titleId, no) {
+                    return 'http://comic.naver.com/webtoon/detail.nhn' + self.searchStr({
+                        titleId: titleId,
+                        no: no
+                    });
+                }
+                if (no === undefined) {
+                    toonaverse.info(titleId).then(function (info) {
+                        resolve(compose(titleId, info.lastNo));
+                    });
+                    return;
+                }
+                resolve(compose(titleId, no));
+            });
+            url.then(function (url) {
+                return self.dom(url);
+            }).then(function (contentDom) {
                 { // title
                     var titleArea = contentDom.getElementsByClassName('tit_area')[0];
                     var title = titleArea.getElementsByTagName('h3')[0].textContent;
@@ -152,24 +174,38 @@ case 'challenge.nhn':
 case 'list.nhn':
     break;
 case 'detail.nhn':
-    launchNuclearBomb().then(function () {
-        return toonaverse.info();
-    }).then(function (info) {
-        var contentPromises = [];
-        for (var i = 1; i <= info.lastNo; ++i)
-            contentPromises.push(toonaverse.content(toonaverse.search.titleId, i));
-        Promise.all(contentPromises).then(function (contents) {
-            document.body.innerHTML = contents.map(function (content) {
-                if (content === null) return '';
-                return [
-                    '<p>제목: ' + content.title + '</p>',
-                    '<p>작가의 말: ' + content.authorComment + '</p>',
-                    content.imgUrls.map(function (imgUrl) {
+    (function () {
+        var currentContent;
+        toonaverse.content().then(function (currentContent) {
+            launchNuclearBomb().then(function () {
+                document.body.innerHTML = [
+                    '<p>제목: ' + currentContent.title + '</p>',
+                    '<p>작가의 말: ' + currentContent.authorComment + '</p>',
+                    currentContent.imgUrls.map(function (imgUrl) {
                         return '<img style="display:block" src="' + imgUrl + '">'
                     }).join('')
                 ].join('');
-            }).join('');
+            });
         });
-    });
+    })();
+    // launchNuclearBomb().then(function () {
+    //     return toonaverse.info();
+    // }).then(function (info) {
+    //     var contentPromises = [];
+    //     for (var i = 1; i <= info.lastNo; ++i)
+    //         contentPromises.push(toonaverse.content(toonaverse.search.titleId, i));
+    //     Promise.all(contentPromises).then(function (contents) {
+    //         document.body.innerHTML = contents.map(function (content) {
+    //             if (content === null) return '';
+    //             return [
+    //                 '<p>제목: ' + content.title + '</p>',
+    //                 '<p>작가의 말: ' + content.authorComment + '</p>',
+    //                 content.imgUrls.map(function (imgUrl) {
+    //                     return '<img style="display:block" src="' + imgUrl + '">'
+    //                 }).join('')
+    //             ].join('');
+    //         }).join('');
+    //     });
+    // });
     break;
 }
